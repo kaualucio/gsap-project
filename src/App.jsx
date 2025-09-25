aef143d7a4d087d91fa9519ba967e0a201d36ad8
kaualucio <kauadefreitas.s992@gmail.com>
2025-09-24 21:51:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+export const App = () => {
+  return <div>App</div>;
+};
